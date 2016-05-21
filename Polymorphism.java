a4f2cf15ba93e7e3924ea053db786f1e8b220250
Max Ponomarenko <maxeleron@gmail.com>
2016-05-21 09:25:09 +0300

    New

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,25 +1,32 @@
-//Ось представлений поліморфізм підтипів.
-class A {
-    void printClassName() {
-        System.out.print("A");
+
+class Animal {
+    String about = "Animal is multicellular, eukaryotic organism. Animal is motile, at some point in their lives. \n\n";
+    void printAboutInfo() {
+        System.out.print(about);
     }
 }
  
-class B extends A  {
-    void printClassName() {
-        System.out.print("B");
+class Mammal extends Animal  {
+    String about = "Mammal has hair, three middle ear bones, and mammary glands. The mammalian brain regulates body temperature and the circulatory system, including the four-chambered heart.\n\n";
+    void printAboutInfo() {
+        System.out.print(about);
     }
 }
  
-class C extends B  {
-    void printClassName() {
-        System.out.print("C");
+class Human extends Mammal  {
+    String about = "Human characterized by erect posture and bipedal locomotion; manual dexterity and increased tool use, compared to other animals; and a general trend toward larger, more complex brains and societies.\n\n";
+    void printAboutInfo() {
+        System.out.print(about);
     }
 }
  
 class Test {
     public static void main(String[] args) {
-        A example = new C ();
-        example.printClassName();
+        Animal example1 = new Human ();
+        Mammal example2 = new Human ();
+        
+        example1.printAboutInfo();
+        example2.printAboutInfo();
+        
     }
 }
